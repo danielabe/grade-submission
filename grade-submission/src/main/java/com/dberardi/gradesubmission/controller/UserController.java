9f397b2f207d373998094a448cb3458c176a57e9
Daniela Berardi <danielaberardi@live.com.ar>
2024-04-23 10:49:53 -0300

    add UserController documentation

--- grade-submission/src/main/java/com/dberardi/gradesubmission/controller/UserController.java
+++ grade-submission/src/main/java/com/dberardi/gradesubmission/controller/UserController.java
@@ -1,10 +1,18 @@
 package com.dberardi.gradesubmission.controller;
 
+import com.dberardi.gradesubmission.exception.ErrorResponse;
 import com.dberardi.gradesubmission.model.User;
 import com.dberardi.gradesubmission.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "User Controller", description = "Manage users")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/user")
@@ -20,12 +29,24 @@ public class UserController {
 
     UserService userService;
 
-    @GetMapping("/{id}")
+    @Operation(summary = "Get user based on ID", description = "Returns a user based on an ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful retrieval of user", content = @Content(schema = @Schema(implementation = String.class))),
+            //@ApiResponse(responseCode = "400", description = "Failed to convert type", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content),
+            //@ApiResponse(responseCode = "404", description = "User doesn't exist", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+    })
+    @GetMapping(value = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> findById(@PathVariable Long id) {
         return new ResponseEntity<>(userService.getUser(id).getUsername(), HttpStatus.OK);
     }
 
-    @PostMapping("/register")
+    @Operation(summary = "Create user", description = "Creates a user from the provided payload")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successful user creation", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+    })
+    @PostMapping(value = "register", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<HttpStatus> createUser(@Valid @RequestBody User user) {
         userService.saveUser(user);
         return new ResponseEntity<>(HttpStatus.CREATED);
